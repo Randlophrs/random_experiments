1f3270e2d143ca67e47844ed37035ec657d99d52
Randlophrs <jansenlimas0301@gmail.com>
2026-03-07 12:08:24 +0700

    update datatables

---
+++ kehati/datatables/script.js
@@ -0,0 +1,52 @@
+
+
+new DataTable("#example", {
+    // columnControl: [
+    //     "order",
+    //     ["searchText", "orderDesc", "orderAsc", "orderRemove"],
+    // ],
+
+    ordering: {
+        indicators: false,
+        handler: false,
+    },
+
+    columns: [
+        {
+            columnControl: [
+                "order",
+                ["searchText", "orderDesc", "orderAsc", "orderRemove"],
+            ],
+        },
+        {
+            columnControl: [
+                "order",
+                ["searchList", "orderDesc", "orderAsc", "orderRemove"],
+            ],
+        },
+        {
+            columnControl: [
+                "order",
+                ["searchText", "orderDesc", "orderAsc", "orderRemove"],
+            ],
+        },
+        {
+            columnControl: [
+                "order",
+                ["searchText", "orderDesc", "orderAsc", "orderRemove"],
+            ],
+        },
+        {
+            columnControl: [
+                "order",
+                ["searchText", "orderDesc", "orderAsc", "orderRemove"],
+            ],
+        },
+        {
+            columnControl: [
+                "order",
+                ["searchText", "orderDesc", "orderAsc", "orderRemove"],
+            ],
+        },
+    ],
+});
